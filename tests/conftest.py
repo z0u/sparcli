05b8a774377ec2052166a445d8a2b575b783d5f3
Alex Fraser <alex.d.fraser@gmail.com>
2020-02-01 15:40:36 +1100

    Not outputting code coverage when not all tests are run

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtestloop(session):
+    yield
+    disable_coverage_when_not_all_tests_were_run(session.config)
+
+
+def disable_coverage_when_not_all_tests_were_run(config):
+    plugin = config.pluginmanager.getplugin('_cov')
+    if plugin and (config.option.keyword or config.option.file_or_dir):
+        plugin.cov_total = None
