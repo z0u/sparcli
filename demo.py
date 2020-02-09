7ce8d55b2f250a438a8d9b9dbed205e19be50f0c
Alex Fraser <alex.d.fraser@gmail.com>
2020-02-09 21:50:48 +1100

    Added a generator wrapper

--- demo.py
+++ demo.py
@@ -2,37 +2,65 @@
 from itertools import count
 import math
 import random
+from threading import Thread
 import time
 
 import numpy as np
 import sparcli
 
 
-def run():
+running = True
+
+
+def demo():
+    jobs = start_producers()
+    try:
+        time.sleep(100)
+    finally:
+        stop_producers(jobs)
+
+
+def start_producers():
+    global running
+    running = True
+    jobs = []
+    for producer in (using_context_manager, using_generator):
+        job = Thread(target=producer, daemon=True)
+        job.start()
+        jobs.append(job)
+    return jobs
+
+
+def stop_producers(jobs):
+    global running
+    running = False
+    for job in jobs:
+        job.join()
+
+
+def using_context_manager():
+    with sparcli.context() as ctx:
+        while running:
+            ctx.record(random=random.random())
+            time.sleep(0.1)
+
+
+def using_generator():
+    for y in sparcli.gen(sinewave(), "sine"):
+        if not running:
+            break
+        time.sleep(0.2)
+
+
+def sinewave():
+    x = 0
+    while True:
+        yield np.sin(x / 10)
+        x += 1
+
+
+if __name__ == "__main__":
     try:
         demo()
     except KeyboardInterrupt:
         pass
-
-
-def demo():
-    with sparcli.sparcli() as s:
-        randoms = take(random.random)
-        sine = sinewave()
-        for x1, x2 in zip(randoms, sine):
-            s.record(random=x1, sine=x2)
-            time.sleep(0.1)
-
-
-def sinewave():
-    for x in count():
-        yield np.sin(x / 10)
-
-
-def take(callable):
-    while True:
-        yield callable()
-
-
-if __name__ == "__main__":
-    run()
--- sparcli/__init__.py
+++ sparcli/__init__.py
@@ -1,29 +1,41 @@
 import atexit
+import threading
 
 from . import render
 from . import services
 
 
 _controller = None
+_controller_lock = threading.Lock()
 
 
 def get_controller():
     global _controller
-    if not _controller:
-        atexit.register(cleanup)
-        renderer = render.Renderer()
-        _controller = services.Controller(renderer)
-        _controller.start()
+    with _controller_lock:
+        if not _controller:
+            print("getting new controller")
+            atexit.register(cleanup)
+            renderer = render.Renderer()
+            _controller = services.Controller(renderer)
+            _controller.start()
     return _controller
 
 
 def cleanup():
     global _controller
-    if _controller:
-        _controller.stop()
-        _controller.join()
-        _controller = None
+    with _controller_lock:
+        if _controller:
+            _controller.stop()
+            _controller.join()
+            _controller = None
 
 
-def sparcli():
+def context():
     return services.Sparcli(get_controller().event_queue)
+
+
+def gen(iterable, name):
+    with context() as ctx:
+        for value in iterable:
+            ctx.record(**{name: value})
+            yield value
